e6c513909d6f667db1513a7f8eb1e48f2da5f8d1
Ben Coman <btc@openInWorld.com>
2019-01-07 00:02:09 +0800

    Nicer local build output

--- travis_build.sh
+++ travis_build.sh
@@ -2,16 +2,16 @@
 
 # In a fresh WSL installation, first do `sudo apt-get install mingw-w64`
 
-mkdir build
+mkdir -p build
 i686-w64-mingw32-gcc hello.c -o build/hello-w32.exe
 x86_64-w64-mingw32-gcc hello.c -o build/hello-w64.exe
+set +x
 
 # Check running locally within WSL, then copy to windows side for testing.
 if uname -a | grep -q 'Microsoft' ; then
 	mkdir -p /mnt/c/tmp/hello
 	cp build/* /mnt/c/tmp/hello
+	echo Now in the Windows shell CMD.EXE do...
+	echo '  cd C:\\tmp\\hello'
+	echo '  hello-w32.exe'
 fi
-
-# Now in CMD.EXE do...
-#   cd C:\tmp\hello
-#   hello-w32.exe
